218cfad60ee4684918d4f770044db8739d90ecf5
Yulia-As-jpg <yulkahitrova26@gmail.com>
2025-01-11 15:08:19 +0300

    11111

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './componens/App/App'
+import App from './component/App/App'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
